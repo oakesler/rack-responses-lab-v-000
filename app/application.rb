01973fe1839633fcfc44e9045f57ede824973a01
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 23:59:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,14 @@
 class Application
   require 'rack'
   
+  require 'pry'
+  
   def call(env)
     return [ 200, {'Content-Type' => 'text/html'}, time_appropriate_greeting]
     end
  
   def time_appropriate_greeting
+    binding.pry
     time = Time.new
     if time.hour.to_i >= 12
       ["<strong>Good Afternoon!</strong>"] 
