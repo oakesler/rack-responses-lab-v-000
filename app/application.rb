1c3cb631749884be555b91adbd5ba568f70168a5
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 23:48:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,24 +1,13 @@
 class Application
-
-  #def call(env)
-    #resp = Rack::Response.new
-    #resp.write "Hello, my name is "
-    #resp.finish
-  #end
-
-#end
-
-
-require 'rack'
- 
-# Something that responds to call, that's what Rack demands
+  require 'rack'
+  
   def call(env)
     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
-  end
+    end
  
   def pretty_response
     (Time.now.to_i >= 12) ?  ["<strong>Good Morning!</strong>"] : ["<strong>Good Afternoon!</strong>"]
   end
 end
  
-run MyServer.new
+run Application.new
