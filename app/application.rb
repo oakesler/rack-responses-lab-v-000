4edb7d8e6b44e119a236d0d02d4f711ae0a1c233
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 23:43:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class MyServer
   end
  
   def pretty_response
-    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+    (Time.now.to_i >= 12) ?  ["<strong>Good Morning!</strong>"] : ["<strong>Good Afternoon!</strong>"]
   end
 end
  
