0f28b6a1a93b3e67b70927fcefa97298cd4aaa39
Olivia Kesler <oakesler@protonmail.ch>
2019-12-15 00:03:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,13 +8,13 @@ class Application
     end
  
   def time_appropriate_greeting
-    binding.pry
     time = Time.new
-    if time.hour.to_i >= 12
-      ["<strong>Good Afternoon!</strong>"] 
-      else 
-        ["<strong>Good Morning!</strong>"]
-      end
+    puts time.hour.to_i
+    #if time.hour.to_i >= 12
+      #["<strong>Good Afternoon!</strong>"] 
+      #else 
+        #["<strong>Good Morning!</strong>"]
+      #end
     end
   end
  
